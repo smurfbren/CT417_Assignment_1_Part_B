68e96b5b7a15872d24565f8dd477b6bebaa1e8ee
smurfbren <smurfbren@gmail.com>
2018-11-01 17:13:22 +0000

    Update Student_Reg_Driver.java

--- Main_B/java/Student_Reg_Driver.java
+++ Main_B/java/Student_Reg_Driver.java
@@ -9,9 +9,13 @@ public class Student_Reg_Driver {
 		
 		Course Eng = new Course("English", LocalDate.parse("2015-7-13"), LocalDate.parse("2018-8-21"));
 		
+		//module details for the english course
+		
 		Module Writing = new Module("Writing" + " ", "CT112");
 		Module Poetry = new Module("Poetry" + " ", "CT123");
 		Module History = new Module("History" + " ", "CT214");
+		
+		//students to attend the course
 		
 		Student Liam = new Student("Liam Morgan", 21, null, 15, Eng);
 		Student Mary = new Student("Mary Red", 19, null, 18, Eng);
@@ -32,6 +36,8 @@ public class Student_Reg_Driver {
 		History.setStudent(Mary);
 		Poetry.setStudent(Mary);
 
+		//adding students to the modules
+		
 		Liam.setModule(History);
 		Liam.setModule(Writing);
 		Liam.setModule(Poetry);
@@ -40,10 +46,12 @@ public class Student_Reg_Driver {
 		Mary.setModule(Writing);
 		Mary.setModule(Poetry);
 		
+		//print out the details of the course such as students attending and modules ID
+		
 		System.out.println(Eng + "\n \n");
-		System.out.println(Writing + "\n \n");
-		System.out.println(History + "\n" + " \n");
-		System.out.println(Poetry + "\n \n");
+		System.out.println(Writing);
+		System.out.println(History);
+		System.out.println(Poetry);
 	
 	}
 }
